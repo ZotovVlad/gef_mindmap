6622c5ea80a0d552f20ab0396c5e83dd79fb2fff
Vlad Zotov <sotowwlad@gmail.com>
2020-08-10 15:44:49 +0400

    clean field

--- com.itemis.gef.tutorial.mindmap.model/src/com/itemis/gef/tutorial/mindmap/model/SimpleMindMapExampleFactory.java
+++ com.itemis.gef.tutorial.mindmap.model/src/com/itemis/gef/tutorial/mindmap/model/SimpleMindMapExampleFactory.java
@@ -19,31 +19,31 @@ public class SimpleMindMapExampleFactory {
 
 		mindMap.addChildElement(center);
 
-		MindMapNode child = null;
-		for (int i = 0; i < 5; i++) {
-			child = new MindMapNode();
-			child.setTitle("Association #" + i);
-			child.setDescription("I just realized, this is related to the core idea!");
-			child.setColor(Color.ALICEBLUE);
-
-			child.setBounds(new Rectangle(50 + (i * 200), 250, WIDTH, 100));
-			mindMap.addChildElement(child);
-
-			MindMapConnection conn = new MindMapConnection();
-			conn.connect(center, child);
-			mindMap.addChildElement(conn);
-		}
-
-		MindMapNode child2 = new MindMapNode();
-		child2.setTitle("Association #4-2");
-		child2.setDescription("I just realized, this is related to the last idea!");
-		child2.setColor(Color.LIGHTGRAY);
-		child2.setBounds(new Rectangle(250, 550, WIDTH, 100));
-		mindMap.addChildElement(child2);
-
-		MindMapConnection conn = new MindMapConnection();
-		conn.connect(child, child2);
-		mindMap.addChildElement(conn);
+//		MindMapNode child = null;
+//		for (int i = 0; i < 5; i++) {
+//			child = new MindMapNode();
+//			child.setTitle("Association #" + i);
+//			child.setDescription("I just realized, this is related to the core idea!");
+//			child.setColor(Color.ALICEBLUE);
+//
+//			child.setBounds(new Rectangle(50 + (i * 200), 250, WIDTH, 100));
+//			mindMap.addChildElement(child);
+//
+//			MindMapConnection conn = new MindMapConnection();
+//			conn.connect(center, child);
+//			mindMap.addChildElement(conn);
+//		}
+//
+//		MindMapNode child2 = new MindMapNode();
+//		child2.setTitle("Association #4-2");
+//		child2.setDescription("I just realized, this is related to the last idea!");
+//		child2.setColor(Color.LIGHTGRAY);
+//		child2.setBounds(new Rectangle(250, 550, WIDTH, 100));
+//		mindMap.addChildElement(child2);
+//
+//		MindMapConnection conn = new MindMapConnection();
+//		conn.connect(child, child2);
+//		mindMap.addChildElement(conn);
 
 		return mindMap;
 	}
